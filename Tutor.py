bdee2bc5ef4b9a9c702fc3ddb880286a1f0dce11
GhostAnonH2A <49526865+GhostAnonH2A@users.noreply.github.com>
2019-07-03 22:36:15 -0500

    Add files via upload

---
+++ Tutor.py
@@ -0,0 +1,19 @@
+# Keren Kan print semua :V makanya harus cerdas ;V
+print('===========================================')
+print('+              #ToolsFB#                  +')
+print('===========================================')
+print('+ BY            : Ghost@AnonH2A           +')
+print('+ Thank         : All Team Cyber          +')
+print('+ Spesial Thank : Semua Kawan-Kawan Saya  +')
+print('===========================================')
+print('++++++++++++++>How To Install<+++++++++++++')
+print('=++++++++++++++++>Linux<++++++++++++++++++=')
+web= 'https://github.com/GhostAnonH2A/ToolsFB.git'
+print('# apt-get install python2') ; print('# apt-get install git') ; print('# pip2 install mechanize requests') ; print('# git clone '+web) ; print('# cd ToolsFB') ; print('# python Fb.py') ;
+print('=+++++++++++++++++++++++++++++++++++++++++=') ;
+print('+=========================================+') ;
+print('+================>Termux<=================+') ;
+print('$ pkg install git') ; print('$ pkg install python2') ; print('$ pip2 installl mechanize requests') ; print('$ git clone '+web) ; print('$ cd ToolsFB') ; print('$ python2 Fb.py') ;
+print('+++++++++++++++++++++++++++++++++++++++++++')
+print('=             #Thanks Ya#                 =')
+print('===========================================')
